55540bb3862465075ede103ee4b8baeaa31bc830
Pranav Raj <pranav.raj@gmail.com>
2023-04-22 03:12:57 +0200

    updated smart-contract with additional functions

--- contracts/RFID.sol
+++ contracts/RFID.sol
@@ -1,43 +1,69 @@
 // SPDX-License-Identifier: ISC
+
 pragma solidity ^0.8.8;
 
 contract RFID {
 
-    uint32 public ProdCounter = 0 ;
-    mapping(uint32 => mapping(string => string)) public VIN_RFID_PARTs ;   // VIN(RFID->Partname)
+    // Array to store all VINs
+    uint32 [] VINs ;
 
+    // Structure to create a product blueprint with all fields
     struct Product {
         string modelName;
         uint32 modelYear;
         string codeInfo;
+        string [] rfids;
     }
-    mapping(uint32 => Product) Products ;    // Mapping for VIN number and Product
+
+    // Mapping for VIN number and Product
+    mapping(uint32 => Product) Products ;
 
     /* function to add product info for given VIN number
        VIN Format: YYYYMMxxxxx e.g-> 202303100
     */
-    function add_VINfo(uint32 _vin, string memory _modelname, uint32 _modelyear, string memory _code) external {
+    function add_VINfo(uint32 _vin, string memory _modelname, uint32 _modelyear, string memory _code, string[] memory _rfids) external {
         Product memory _prod = Product({modelName : _modelname,
                                         modelYear : _modelyear ,
-                                        codeInfo  : _code});
+                                        codeInfo  : _code,
+                                        rfids     : _rfids});
         Products[_vin] = _prod;
-        ProdCounter += 1;
+        VINs.push(_vin);
+    }
+
+    // Read vehicle metadata by VIN number
+    function get_matadata_byVIN (uint32 _vin) public view returns (string memory, uint32, string memory) {
+        return (Products[_vin].modelName, Products[_vin].modelYear, Products[_vin].codeInfo);
+    }
+
+    // Read all RFIDs by VIN number
+    function get_all_rfids(uint32 _vin) external view returns (string [] memory) {
+        return Products[_vin].rfids ;
+    }
+    
+    // Total registered VINs
+    function product_count() external view returns (uint) {
+        return VINs.length ;
+    }
+    
+    // Update RFID of a VIN
+    function update_rfid_byVIN(uint32 _vin, string memory _old_rfid, string memory _new_rfid) external returns (bool) {
+        string[] memory _tempArray = Products[_vin].rfids ;
+        for (uint i=0; i< _tempArray.length; i++){
+            if (keccak256(abi.encodePacked(_tempArray[i])) == keccak256(abi.encodePacked(_old_rfid))){
+                (Products[_vin].rfids)[i] = _new_rfid;
+                return true;
+            } else {
+                return false ;
+            }
+
+        }
     }
 
 
-    // Add RFID and part info manually
-    function register_RFid_parts (uint32 _vin, string memory _rfid, string memory _partname) external {
-        VIN_RFID_PARTs[_vin][_rfid] = _partname ;
+    // Add - additional RFID to an existing VIN (one at a time)
+    function add_rfid_toVIN(uint32 _vin, string memory _add_rfid) external returns (bool){
+        (Products[_vin].rfids).push(_add_rfid);
+        return true;
     }
 
-    // Read mapping info
-    function read_VINfo (uint32 _vin) public view returns (Product memory) {
-        return Products[_vin] ;
-    }
-
-    // Fetch all rfid for a given VIN
-    // function fetch_RFIDs(uint32 _vin) public view returns (string[] memory, string[] memory) {
-    //    string [] memory = new 
-    //    return VIN_RFID_PARTs[_vin] ;
-    // }
 }
